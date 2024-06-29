894a174224c8087a54c9c0aabef31fd08815763d
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2024-06-29 18:39:26 +0900

    Add OpenAI integration and improve project setup (#1)
    - Integrated OpenAI's API for generating concise titles and descriptions
    from commit messages and diffs.
    - Added `.flake8` configuration with a max line length of 150.
    - Updated `.gitignore` to exclude common Python and project-specific
    files.
    - Added MIT License to the repository.
    - Updated `pyproject.toml` to include dependencies for `openai`,
    `python-dotenv`, and `click`.
    - Added `sum-diff` script to `poetry` scripts for generating summaries.
    - Implemented `git_current_branch`, `git_parent_branch`,
    `git_diff_from_parent`, and `git_logs_from_parent` functions in
    `sum_diff/git.py` for Git operations.
    - Refactored code for clarity and consistency, including handling of
    parent branches in Git diff commands.

---
+++ sum_diff/__init__.py
@@ -0,0 +1,105 @@
+import os
+import click
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from sum_diff.git import (
+    git_current_branch,
+    git_parent_branch,
+    git_diff_from_parent,
+    git_logs_from_parent,
+)
+
+load_dotenv(".env")
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+# Pull request best practices
+# https://blog.pragmaticengineer.com/pull-request-or-diff-best-practices/
+SYSTEM_PROMPT = """
+You are a software engineer who has been asked to explain your changes.
+
+You will be provided with:
+
+- Commit messages
+- Diff of the changes
+
+and you need to explain them in a concise title and description.
+
+Follow these steps to generate an appropriate title and description:
+
+1. Read the _Commit messages_ to understand, in order, what changes have been made in this branch.
+2. Read the _Diff_ to understand how the changes were made. Extracting some key points to generate a title and description.
+3. Generate an appropriate title and description following the guidelines below.
+
+## Guidelines for clear and concise title
+
+### Starting the title with uppercase and the first word being a verb in present tense.
+
+- GOOD: "Change Swiss tax calculation for new regulation"
+- NOT GOOD: "changed Swiss tax calculation for new regulation"
+- NOT GOOD: "Ability to calculate Swiss tax for new regulation"
+
+### Keep the title short, but expressive enough to signal what the reviewer can expect.
+
+Let's say you're working on an accounting software and there is a new business requirement on how to
+calculate tax for Switzerland.
+
+- GOOD: A title like "Changes for Swiss tax calculation" is concise.
+- GOOD: A title like "Change for tax calculation in Switzerland for new regulation" is still
+  concise, with more context.
+- BAD: A title like "Adding new tax parameters" or "Change tax logic" are overly generic and won't
+  help reviewers.
+- BAD: "Changes for calculation" is too generic.
+- NOT GOOD: "TaxInternals structure update and CalculateEffectiveRate changes in case of Swiss
+  country code" is too detailed and should be in the description, not the title.
+- NOT GOOD: "Change tax calculation for Swiss businesses in the TX bracket effective from 2019
+  following new regulations" is too verbose.
+
+## Output Format
+
+- Don't surround title with quotes
+- Enclose programming language elements in backticks (e.g., `if`, `while`).
+"""
+
+USER_PROMPT = """
+## Commit messages
+
+{logs}
+
+## Diff
+
+{diff}
+"""
+
+
+@click.command()
+def main():
+    current_branch = git_current_branch()
+    parent_branch = git_parent_branch(current_branch)
+    diff = git_diff_from_parent(parent_branch)
+    logs = git_logs_from_parent(parent_branch)
+    print(diff)
+    print(logs)
+
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        # model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT.format(logs=logs, diff=diff)},
+            {
+                "role": "user",
+                "content": USER_PROMPT.format(logs=logs, diff=diff),
+            },
+        ],
+        temperature=0.0,
+    )
+
+    title = completion.choices[0].message.content
+
+    if title:
+        title = title.strip()
+        print(title)
